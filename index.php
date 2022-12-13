7de3f51b0df6db978df7e2686a65e63a99bf2fbe
Iunora93 <eleonoraruggieri22@gmail.com>
2022-12-13 15:00:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+$text = 'Ciao a tutti oggi è stato il mio primo giorno di backend!😊';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <p><?php echo $text; ?></p>
+</body>
+</html>
+
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
